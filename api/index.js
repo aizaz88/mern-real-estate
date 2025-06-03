8bdaaa77ddca1d7f0c8216ea3defa166f6bb7df8
asadullah <asadullah37561@gmail.com>
2025-06-03 23:22:18 +0500

    MongoDB create and connect

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from "express";
-const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+//////////////////
+//Mongo connect -----
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+//////////////////////////////////////////
+const app = express();
 app.listen(5000, () => {
-  console.log("server is listening on port 5000!!!!");
+  console.log("server is listening on port 5000!!");
 });
