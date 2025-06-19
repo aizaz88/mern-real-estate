dc0e314b6fd80f0e6b6a69420181a24dc89e6893
asadullah <asadullah37561@gmail.com>
2025-06-19 11:12:46 +0500

    didi fixes on some bugs

--- api/index.js
+++ api/index.js
@@ -7,7 +7,6 @@ import uploadRouter from "./routes/upload.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
 import path from "path";
-
 //////////////////////
 //Directory name
 const __dirname = path.resolve();
@@ -28,7 +27,6 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/upload", uploadRouter);
 app.use("/api/listing", listingRouter);
-
 app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
