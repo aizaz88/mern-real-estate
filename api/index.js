94a7ab4f613c21f79915acfd8a387de4b7b94d6b
asadullah <asadullah37561@gmail.com>
2025-06-23 23:55:22 +0500

    / route

--- api/index.js
+++ api/index.js
@@ -6,10 +6,9 @@ import authRouter from "./routes/auth.route.js";
 import uploadRouter from "./routes/upload.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
-import path from "path";
+
 //////////////////////
 //Directory name
-const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -27,9 +26,8 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/upload", uploadRouter);
 app.use("/api/listing", listingRouter);
-app.use(express.static(path.join(__dirname, "/client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 /////////////////////
 //MiddleWare (error)
